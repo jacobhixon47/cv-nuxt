4efc60852d74dcc194c0b725e94fb36beccc274e
Jacob <jacobhixon47@gmail.com>
2021-06-15 18:53:30 -0500

    initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-fixed w-screen h-screen bg-cover" :style="{ backgroundImage: `url(${image})`}">
+
+  </div>
+</template>
+
+<script>
+import dayOneImg from 'assets/day-1.jpg'
+import dayTwoImg from 'assets/day-2.jpg'
+import dayThreeImg from 'assets/day-3.jpg'
+import nightOneImg from 'assets/night-1.jpg'
+
+let dayImages = [dayOneImg, dayTwoImg, dayThreeImg]
+let nightImages = [nightOneImg]
+
+let now = new Date().getHours();
+let currentImg;
+
+if (now <= 20 || now >= 5) {
+  currentImg = dayImages[Math.floor(Math.random()*dayImages.length)]
+} else {
+  currentImg = nightImages[Math.floor(Math.random()*nightImages.length)]
+}
+
+  export default {
+    data() {
+      return {
+        image: currentImg,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
